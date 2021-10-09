843589ce2347004faa127c1b21345fbed9a33b09
becs <angpohye@gmail.com>
2021-10-09 12:47:39 +0800

    Product List and Product Card

--- src/views/products/index.jsx
+++ src/views/products/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Container, Row, Col} from 'react-grid-system';
+import Grid from '@mui/material/Grid';
+import Content from "components/product-card";
 
 const Products = () => {
     return(
-        <div>
-            hello welcome to our product page.
-        </div>
+        <Grid container direction="column">
+            <Grid item> Product List Below</Grid>
+            <Grid item container> 
+                <Grid item xs={0} sm={1}/>{/* left side margins */}
+                <Grid item xs={12} sm={10}>
+                    <Content/>   
+                </Grid>
+                <Grid item xs={0} sm={1}/>{/* right side margins*/}
+            </Grid>
+        </Grid>
     )
 }
 
