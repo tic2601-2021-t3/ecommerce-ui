9505e8a8ada1b11383eab5e7fd6e9d26a52747a7
becs <angpohye@gmail.com>
2021-10-15 03:51:04 +0800

    no message

--- ui/src/views/products/product-details/index.jsx
+++ ui/src/views/products/product-details/index.jsx
@@ -12,20 +12,36 @@ const ProductDetails = () => {
                 <Grid item xs={6}> 
                     <img src="https://unsplash.com/photos/PZ57Npp7iAw"/>
                 </Grid>
+                <Grid item xs={6} container> 
+                    <Grid item xs={4}>Name of Product</Grid>
+                    <Grid item xs={2}>$36</Grid>
+                    {/*<ProductCard/>*/}
+                    <Grid item xs={6}></Grid>
+                    <Grid item xs={12}>By merchant name</Grid>
 
-                <Grid item xs={1}></Grid>
-                <Grid item xs={2}>Name of Product</Grid>
-                <Grid item xs={2}>$36</Grid>
-                {/*<ProductCard/>*/}
-                <Grid item xs={1}></Grid>
+                    <Grid item xs={6}></Grid>
+                    <Grid item xs={4}>
+                        <Button size="small">Buy</Button>
+                    </Grid>
+                    
+                </Grid>
+            </Grid>
 
-                <Grid item xs={6}></Grid>
-                <Grid item xs={1}></Grid>
-                <Grid item xs={2}>By merchant name</Grid>
-                <Grid item xs={3}></Grid>
+            <Grid container>
+                <Grid item xs={12}>Product Description</Grid>
+                <Grid item xs={12}>Other information about the product</Grid>
+                <Grid item xs={12} container> 
+                    <Grid item xs={4}>Product Id</Grid>
+                    <Grid item xs={2}>$36</Grid>
+                    <Grid item xs={6}></Grid>
 
-                <Grid item xs={6}></Grid>
-                <Button size="small">Buy</Button>
+                    <Grid item xs={4}>Available</Grid>
+                    <Grid item xs={2}>Product Qty</Grid>      
+                    <Grid item xs={6}></Grid>              
+                </Grid>
+            </Grid>
+            <Grid container>
+                user review
             </Grid>
         </div>
     )
