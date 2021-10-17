cd78e0f5ed0c9a0d308c2e3cd59f2fa3f54da816
becs <angpohye@gmail.com>
2021-10-17 20:45:32 +0800

    Finishing up product details page. Still wip.

--- ui/src/views/products/product-details/index.jsx
+++ ui/src/views/products/product-details/index.jsx
@@ -16,59 +16,29 @@ const ProductDetails = () => {
                         <img src="https://unsplash.com/photos/PZ57Npp7iAw"/>
                     </Card>
                 </Col>
-                {/* <Col md={6}>
-                    <div>
-                        <Row>
-                            <Col>
-
-                            </Col>
-                        </Row>
-                    </div>
-                </Col> */}
             </Row>
+                <Col md={4}>Name of Product</Col> <Col md={1}> : </Col> <Col md={4}>Sunflower Shirt</Col> <Col md={3}> </Col>
+                <Col md={4}>Price</Col> <Col md={1}> : </Col> <Col md={4}>$12</Col> <Col md={3}> </Col>
+                <Col md={4}>Description</Col> <Col md={1}> : </Col> <Col md={4}>Sweat wicking material. Authentic!</Col> <Col md={3}> </Col>
+                
+                <Button 
+                    className={styles.btnWrapper}
+                    size="large" 
+                    variant="contained"
+                    //onClick={onHandleLogin}
+                    href="#">
+                    Buy
+                </Button>
+
+                <Col md={4}>Merchant</Col> <Col md={1}> : </Col> <Col md={4}>Lululemon</Col> <Col md={3}> </Col>
+                <Col md={4}>Available Stock</Col> <Col md={1}> : </Col> <Col md={4}>216</Col> <Col md={3}> </Col>
             <Row>
-            
+                <Col md={12}>Product Reviews</Col> 
+                <Col md={4}>User1</Col> <Col md={1}> rated </Col> <Col md={7}>* * * * *</Col> 
+                <Col md={4}>User2</Col> <Col md={1}> rated </Col> <Col md={7}>* * * * *</Col> 
+                <Col md={4}>User3</Col> <Col md={1}> rated </Col> <Col md={7}>* * * * *</Col> 
             </Row>
         </Container>  
-
-
-
-            // <Grid container>
-            //     <Grid item xs={6}> 
-            //         <img src="https://unsplash.com/photos/PZ57Npp7iAw"/>
-            //     </Grid>
-            //     <Grid item xs={6} container> 
-            //         <Grid item xs={4}>Name of Product</Grid>
-            //         <Grid item xs={2}>$36</Grid>
-            //         {/*<ProductCard/>*/}
-            //         <Grid item xs={6}></Grid>
-            //         <Grid item xs={12}>By merchant name</Grid>
-
-            //         <Grid item xs={6}></Grid>
-            //         <Grid item xs={4}>
-            //             <Button size="small">Buy</Button>
-            //         </Grid>
-                    
-            //     </Grid>
-            // </Grid>
-
-            // <Grid container>
-            //     <Grid item xs={12}>Product Description</Grid>
-            //     <Grid item xs={12}>Other information about the product</Grid>
-            //     <Grid item xs={12} container> 
-            //         <Grid item xs={4}>Product Id</Grid>
-            //         <Grid item xs={2}>$36</Grid>
-            //         <Grid item xs={6}></Grid>
-
-            //         <Grid item xs={4}>Available</Grid>
-            //         <Grid item xs={2}>Product Qty</Grid>      
-            //         <Grid item xs={6}></Grid>              
-            //     </Grid>
-            // </Grid>
-            // <Grid container>
-            //     user review
-            // </Grid>
-
     )
 };
 
